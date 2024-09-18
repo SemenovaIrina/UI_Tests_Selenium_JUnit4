e3c4ecfc4d2c8bfff7caa230d107f51454433ef4
Irina Semenova <semenova.iv.ssau@ya.ru>
2024-09-18 22:46:44 +0400

    load project

---
+++ src/main/java/edu/praktikum/sprint4/pages/HomePage.java
@@ -0,0 +1,82 @@
+package edu.praktikum.sprint4.pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class HomePage {
+    private final WebDriver driver;
+    //локатор для кнопки Заказать вверху
+    private final By orderButtonTop = By.className("Button_Button__ra12g");
+    //локатор для кнопки Заказать внизу
+    private final By orderButtonBottom = By.xpath(".//button[@class='Button_Button__ra12g Button_Middle__1CSJM']");
+    // локатор для элемента выпадающего списка
+    private By itemQuestionsHeading;
+    // локатор для текста, соответствующего элементу выпадающего списка
+    private By itemAnswer;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //сетер, выбирающий элемент выпадающего списка с заданным заголовком, а также текст ответа, соответствующий ему
+    public void setItemQuestionsAndAnswer(String itemText) {
+        String param = String.format(".//div[@class='accordion__button' and text() ='%1$s']",itemText);
+        itemQuestionsHeading = By.xpath(param);
+        List<WebElement> element = driver.findElements(itemQuestionsHeading);
+        if (!element.isEmpty()) {
+            String id = element.get(0).getAttribute("id");
+            itemAnswer = By.id("accordion__panel-" + id.charAt(id.length() - 1));
+        } else {
+            itemAnswer = null;
+        }
+    }
+
+    // метод ожидания загрузки элементов выпадающего списка
+    public void waitForLoadQuestions() {
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions.visibilityOfElementLocated(By.className("accordion__button")));
+    }
+
+    // метод кликает по элементу выпадающего списка с заданным текстом
+    public void clickItemQuestionsHeading() {
+        Assert.assertTrue("Элемент списка с заданным заголовком не найден на странице", driver.findElement(itemQuestionsHeading).isEnabled());
+        //прокручиваем страницу до нужного элемента списка
+        WebElement item = driver.findElement(itemQuestionsHeading);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", item);
+        item.click();
+    }
+
+    // метод для получения текста ответа для элемента выпадающего списка (текст возвращается только если ответ отображается)
+    public String getItemAnswerText() {
+        Assert.assertTrue("Текст ответа не отображается на странице", driver.findElement(itemAnswer).isDisplayed());
+        return driver.findElement(itemAnswer).getText();
+    }
+
+    //метод нажатия на кнопку Заказать вверху экрана
+    public void clickOrderButtonTop() {
+        Assert.assertTrue("Кнопка Заказать не доступна вверху страницы", driver.findElement(orderButtonTop).isEnabled());
+        //прокручиваем страницу до нужного элемента списка
+        WebElement item = driver.findElement(orderButtonTop);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", item);
+        item.click();
+    }
+
+    //метод нажатия на кнопку Заказать внизу экрана
+    public void clickOrderButtonBottom() {
+        Assert.assertTrue("Кнопка Заказать не доступна вверху страницы", driver.findElement(orderButtonBottom).isEnabled());
+        //прокручиваем страницу до нужного элемента списка
+        WebElement item = driver.findElement(orderButtonBottom);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", item);
+        item.click();
+    }
+    //технически можно было бы описать один метод click и передавать в него объект класса By
+    //но подумалось, что расплатой за краткость кода будет потеря прозрачности и понятности
+    //а также отсутствие возможности понять с какой именно кнопкой проблемы
+}
---
+++ src/test/java/MakeOrderTests.java
@@ -0,0 +1,105 @@
+import edu.praktikum.sprint4.pages.AboutRentPage;
+import edu.praktikum.sprint4.pages.HomePage;
+import edu.praktikum.sprint4.pages.OrderPage;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@RunWith(Parameterized.class)
+public class MakeOrderTests {
+    private final String name;
+    private final String surname;
+    private final String address;
+    private final int metroStationItem;
+    private final String telephone;
+    private final String date;
+    private final int periodItem;
+    private final String color;
+    private final String comments;
+    private WebDriver driver;
+
+    public MakeOrderTests(String name, String surname, String address, int metroStationItem, String telephone, String date, int periodItem, String color, String comments) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.metroStationItem = metroStationItem;
+        this.telephone = telephone;
+        this.date = date;
+        this.periodItem = periodItem;
+        this.color = color;
+        this.comments = comments;
+    }
+
+    @Before
+    public void prepare() {
+        // Запускаем браузер, переходим на сайт
+        driver = new ChromeDriver();
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    // Тестовые данные
+    @Parameterized.Parameters
+    public static Object[][] getOrderData() {
+        return new Object[][]{
+                //обязательный формат даты доставки dd.mm.yyyy
+                //парамтеры:
+                //имя & фамилия & адрес & номер_станции_метро_в_списке & телефон & дата_доставки & номер_периода_в_списке & цвет & комментарий
+                {"Иван", "Петров", "Самара, ул.Гагарина, 9", 1, "89271112233", "07.10.2024", 1, "black", ""},
+                {"Ирина", "Семенова", "ул.Мира, 8889", 2, "89271114455", "20.12.2024", 2, "", "В первой половине дня"},
+        };
+    }
+
+    @Test
+    public void makeOrderPositiveTestTopButton() {
+        HomePage page = new HomePage(driver);
+        //ждем когда прогрузятся элементы
+        page.waitForLoadQuestions();
+        //клик по кнопке Заказать вверху страницы
+        page.clickOrderButtonTop();
+        //все действия, которые должны произойти после нажатия на кнопку для оформления заказа
+        afterClickOrderButton();
+    }
+
+    @Test
+    public void makeOrderPositiveTestButtomButton() {
+        HomePage page = new HomePage(driver);
+        //ждем когда прогрузятся элементы
+        page.waitForLoadQuestions();
+        //клик по кнопке Заказать внизу страницы
+        page.clickOrderButtonBottom();
+        //все действия, которые должны произойти после нажатия на кнопку для оформления заказа
+        afterClickOrderButton();
+    }
+
+    public void afterClickOrderButton() {
+        //1-й этап оформления заказа
+        OrderPage orderPage = new OrderPage(driver);
+        //ждем пока загрузится страница
+        orderPage.waitForLoadPage();
+        //заполяем поля формы
+        orderPage.makeOrder(name, surname, address, metroStationItem, telephone);
+        //нажимаем кнопку Далее
+        orderPage.clickThenButton();
+        //2-й этап оформления заказа
+        AboutRentPage nextOrderPage = new AboutRentPage(driver);
+        nextOrderPage.waitForLoadPage();
+        //заполнение полей формы
+        nextOrderPage.finishOrder(date, periodItem, color, comments);
+        //нажатие на кнопку Заказать
+        nextOrderPage.clickOrderButton();
+        //подтверждаем оформление заказа нажатием кнопки Да
+        nextOrderPage.clickYesButton();
+        Assert.assertTrue("Окно не доступно", nextOrderPage.checkIsVisibleModalOrderPlaced());
+    }
+
+    @After
+    public void pageDown() {
+        // Закрываем браузер
+        driver.quit();
+    }
+}
